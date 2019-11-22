1e32957d1231fde6af9ee2292dafabec02c27488
JaronSmi <46786626+JaronSmi@users.noreply.github.com>
2019-11-21 21:19:28 -0500

    Add blog page, add/parse rss feed, add link on home page

--- test_podcast/Controllers/HomeController.cs
+++ test_podcast/Controllers/HomeController.cs
@@ -31,6 +31,10 @@ namespace test_podcast.Controllers
         {
             return View();
         }
+        public IActionResult Blog()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
