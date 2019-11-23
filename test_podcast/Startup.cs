b3247097e620618018b41e0b6b0759c4a11256b6
abwelch <abw28@zips.uakron.edu>
2019-11-23 11:28:15 -0500

    Add connection string to json and configure service to startup

--- test_podcast/Startup.cs
+++ test_podcast/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using test_podcast.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace test_podcast
 {
@@ -24,6 +26,8 @@ namespace test_podcast
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<LoginContext>(options => 
+                    options.UseSqlServer(Configuration.GetConnectionString("LoginContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
