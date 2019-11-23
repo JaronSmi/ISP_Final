828b8d2dfc9024d3858cda7c3fb166133e6fde85
abwelch <abw28@zips.uakron.edu>
2019-11-23 15:05:42 -0500

    Add db context

---
+++ test_podcast/Data/LoginContext.cs
@@ -0,0 +1,12 @@
+﻿
+namespace test_podcast.Data
+{
+    public class LoginContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public LoginContext (Microsoft.EntityFrameworkCore.DbContextOptions<LoginContext> options)
+            : base(options)
+        {}
+        
+        public Microsoft.EntityFrameworkCore.DbSet<test_podcast.Models.Login> User { get; set; }
+    }
+}
---
+++ test_podcast/Models/Login.cs
@@ -0,0 +1,11 @@
+﻿
+namespace test_podcast.Models
+{
+    public class Login
+    {
+        public int id { get; set; }
+        public string email { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
