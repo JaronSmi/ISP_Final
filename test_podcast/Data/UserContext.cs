9c4e4686746fc1f1f6953e7f854ab0a9a2095caf
abwelch <abw28@zips.uakron.edu>
2019-11-23 10:50:33 -0500

    Create User database context class

---
+++ test_podcast/Data/UserContext.cs
@@ -0,0 +1,12 @@
+﻿
+namespace test_podcast.Data
+{
+    public class UserContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public UserContext (Microsoft.EntityFrameworkCore.DbContextOptions<UserContext> options)
+            : base(options)
+        {}
+        
+        public Microsoft.EntityFrameworkCore.DbSet<test_podcast.Models.User> User { get; set; }
+    }
+}
