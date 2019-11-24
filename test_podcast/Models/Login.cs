7d2ae2168ac97fe804b65c5247073fc687834d98
abwelch <abw28@zips.uakron.edu>
2019-11-23 20:27:26 -0500

    Add constraints to login members

--- test_podcast/Models/Login.cs
+++ test_podcast/Models/Login.cs
@@ -1,11 +1,18 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
+
+
 namespace test_podcast.Models
 {
     public class Login
     {
         public int id { get; set; }
+
         public string email { get; set; }
+
+        [StringLength(20, MinimumLength = 4)]
         public string username { get; set; }
+
+        [StringLength(25, MinimumLength = 10)]
         public string password { get; set; }
     }
 }
