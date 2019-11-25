f14a1954384ca9a22e8374a52912bfd0b992ca3d
abwelch <abw28@zips.uakron.edu>
2019-11-24 20:16:40 -0500

    Add random changes

---
+++ test_podcast/Models/Quiz.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace test_podcast.Models
+{
+    public class Quiz
+    {
+        // Primary Key
+        [Required]
+        public int id { get; set; }
+
+        [Required]
+        public int score { get; set; }
+
+        [Required]
+        public System.DateTime date { get; set; }
+
+        [Required]
+        public string username { get; set; }
+    }
+}
---
+++ test_podcast/Models/Username.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace test_podcast.Models
+{
+    public class Username
+    {
+        public static string Name { get; set; }
+    }
+}
